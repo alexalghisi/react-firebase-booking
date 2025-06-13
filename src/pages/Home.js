e3ecdd06b2c285788230bb831086317869520757
Alghisi Alessandro Paolo <alexalghisi@yahoo.com>
2025-06-13 10:05:12 +0300

    chore(firebase): install SDK & bootstrap Firebase + Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import {useEffect, useState} from 'react';
+
 export default function Home() {
-  return (
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+      async function load() {
+          try {
+              const res = await fetch('https://randomuser.me/api/');
+              const json = await res.json();
+              setData(json);
+          } catch (error) {
+              console.error(error);
+          }
+      }
+      load();
+  },[]);
+    return (
     <div className="h-screen flex items-center justify-center">
       <h1 className="text-3xl font-semibold">React Firebase Booking</h1>
+        {!data && <p>Loading...</p>}
+
+        {data && (
+            <pre>{JSON.stringify(data,null,2)}</pre>
+        )}
+
+
+
     </div>
   );
 }
