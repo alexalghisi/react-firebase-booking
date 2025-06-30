ab944dc3fa2d8d1cb6aa9c7e2ab2ca48d18b65c2
Alghisi Alessandro Paolo <alexalghisi@yahoo.com>
2025-06-30 13:51:42 +0300

    Added private routing so only logged-in users can access the booking calendar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
