386fb8dd093542abf97d494b9fc7aec16f9cdc10
Alghisi Alessandro Paolo <alexalghisi@yahoo.com>
2025-07-01 09:12:58 +0300

    Add google auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
+// Your Firebase config from environment variables
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,7 +12,12 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth       = getAuth(app);
-export const db         = getFirestore(app);
-export const googleAuth = new GoogleAuthProvider();
+
+// Auth & Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Google provider
+export const provider = new GoogleAuthProvider();
