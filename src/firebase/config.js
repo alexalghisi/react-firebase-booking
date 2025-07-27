a107ee153e78bc4ba51e269b0e26a45133751c90
Alghisi Alessandro Paolo <alexalghisi@gmail.com>
2025-07-27 21:36:49 +0300

    Fix firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,25 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBGdoqZ8kxZPV7crJDohkPfg3Q8vcwTA-U",
-  authDomain: "react-booking-system-98447.firebaseapp.com",
-  projectId: "react-booking-system-98447",
-  storageBucket: "react-booking-system-98447.firebasestorage.app",
-  messagingSenderId: "1057693391901",
-  appId: "1:1057693391901:web:ec7aa85862a6495afffaf4",
-  measurementId: "G-L8M186C6ET"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
-
 export default app;
